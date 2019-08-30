a576cd2bb0aa16a121b3436a6e8cc11aedab7dee
fD-V <fv@math.u-bordeaux.fr>
2019-08-30 15:28:54 +0200

    cov

--- src/LotSizing/model.jl
+++ src/LotSizing/model.jl
@@ -55,7 +55,7 @@ function model(data::DataSmMiLs, optimizer)
                 )
 
     @constraint(mils, cov[i in I,   s in S], 
-                sum(x[i, i, l, s] for  l in T) >= 1
+                sum(x[i, i, l, s] for  l in i:data.nbperiods) >= 1
                 )
 
     last = data.nbperiods-1
