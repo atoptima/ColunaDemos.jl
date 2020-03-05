1dc0d4c0784d42ec5fd85dc0786774a37dd2c892
Guillaume Marques <guillaume.marques@protonmail.com>
2020-03-05 19:56:41 +0100

    Updates to support unreleased features of BlockDecomposition (#4)
    * update specifies
    
    * new model to test pricing oracles

--- src/GeneralizedAssignment/model.jl
+++ src/GeneralizedAssignment/model.jl
@@ -16,7 +16,7 @@ function model(data::Data, optimizer)
 
     @dantzig_wolfe_decomposition(gap, dec, M)
     subproblems = BlockDecomposition.getsubproblems(dec)
-    specify!(subproblems, lower_multiplicity = 0)
+    specify!.(subproblems, lower_multiplicity = 0)
     return gap, x, dec
 end
 
@@ -47,7 +47,7 @@ function model_with_penalties(data::Data, optimizer)
 
     @dantzig_wolfe_decomposition(gap, dec, M)
     subproblems = BlockDecomposition.getsubproblems(dec)
-    specify!(subproblems, lower_multiplicity = 0)
+    specify!.(subproblems, lower_multiplicity = 0)
 
     return gap, x, y, dec
 end
@@ -73,7 +73,7 @@ function model_with_penalty(data::Data, optimizer)
 
     @dantzig_wolfe_decomposition(gap, dec, M)
     subproblems = BlockDecomposition.getsubproblems(dec)
-    specify!(subproblems, lower_multiplicity = 0)
+    specify!.(subproblems, lower_multiplicity = 0)
 
     return gap, x, y, dec
 end
@@ -96,8 +96,25 @@ function model_max(data::Data, optimizer)
 
     @dantzig_wolfe_decomposition(gap, dec, M)
     subproblems = BlockDecomposition.getsubproblems(dec)
-    specify!(subproblems, lower_multiplicity = 0)
+    specify!.(subproblems, lower_multiplicity = 0)
     
     return gap, x, dec
 end
 
+function model_without_knp_constraints(data::Data, optimizer)
+    gap = BlockModel(optimizer, bridge_constraints = false)
+
+    @axis(M, data.machines)
+
+    @variable(gap, x[m in M, j in data.jobs], Bin)
+
+    @constraint(gap, cov[j in data.jobs],
+            sum(x[m,j] for m in M) >= 1)
+
+    @objective(gap, Min,
+            sum(data.cost[j,m]*x[m,j] for m in M, j in data.jobs))
+
+    @dantzig_wolfe_decomposition(gap, dec, M)
+    
+    return gap, x, dec
+end
