0d2d34364f4d747844648561b6a5d292208d3108
Guillaume Marques <guillaume.marques@protonmail.com>
2020-04-26 13:03:14 +0200

    UFC demo

---
+++ src/UncapacitatedFacilityLocation/data.jl
@@ -0,0 +1,43 @@
+mutable struct Data
+    facilities::UnitRange{Int}
+    cities::UnitRange{Int}
+    openingcost::Vector{Int}
+    linkingcost::Matrix{Int}
+end
+
+function Data(nbf::Int, nbc::Int) 
+    return Data(1:nbf, 1:nbc, zeros(Int, nbf), zeros(Int, nbf, nbc))
+end
+
+function data(filename::AbstractString)
+    data = Int[]
+    filepath = string(@__DIR__ , "/instances/" , filename)
+    open(filepath) do file
+        for line in eachline(file)
+            match(r"^FILE", line) === nothing && continue #  jump file name
+            for pieceofdata in split(line)
+                push!(data, parse(Int, pieceofdata))
+            end
+        end
+    end
+
+    nbfacilites = data[1]
+    nbcities = data[2]
+    zero = data[3]
+
+    dataufl = Data(data[1], data[2])
+
+    offset = 3
+    
+    for f in data.facilities
+        dataufl.openingcost[f] = data[offset + 1]
+        dataufl.linkingcost[f, :] = data[offset + 2 : offset + 2 + nbcities]
+        offset += 2 + nbcities
+    end
+    return dataufl
+end
+
+function show(io::IO, d::Data)
+    println(io, "Uncapacitated Facility Location dataset.")
+    println(io, "nb facilities = $(length(d.facilities)) and nb cities = $(length(d.cities))")
+end
---
+++ src/UncapacitatedFacilityLocation/model.jl
@@ -0,0 +1,19 @@
+function model(data::Data, optimizer)
+    ufl = BlockModel(optimizer, bridge_constraints = false)
+
+    @axis(F, data.facilities)
+
+    @variable(ufl, x[f in F, c in data.cities], Bin) # 1 if f supplies c
+    @variable(ufl, y[f in F], Bin) # 1 if y open
+
+    @constraint(ufl, cov[c in data.cities], sum(x[f,c] for f in F) == 1)
+
+    @constraint(ufl, open[f in F, c in data.cities], x[f,c] <= y[f])
+
+    @objective(ufl, Min,
+        sum(data.linkingcost[f,c]*x[f,c] for f in F, c in data.cities) + sum(data.openingcost[f]*y[f] for f in F)
+    )
+
+    @benders_decomposition(ufl, dec, F)
+    return ufl, x, y, dec
+end
