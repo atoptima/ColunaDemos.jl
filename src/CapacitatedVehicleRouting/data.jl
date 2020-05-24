4279b4c362d38370c12c30b9b86c2ef330417bd6
Guillaume Marques <guillaume.marques@protonmail.com>
2020-05-24 16:30:31 +0200

    data

--- src/CapacitatedVehicleRouting/data.jl
+++ src/CapacitatedVehicleRouting/data.jl
@@ -11,6 +11,21 @@ function data(filename::AbstractString)
     filepath = string(@__DIR__, "/instances/", filename)
     open(filepath) do file
         for line in eachline(file)
+            m = match(r"CAPACITY : (\d+)", line)
+            if length(m.captures) == 1
+                capacity = m.captures[1]
+                continue
+            end
+            m = match(r"^(\d+) (\d+)", line)
+            if length(m.captures) == 2
+                push!(demands, m.captures[2])
+                continue
+            end
+            m = match(r"^ (\d+) (\d+) (\d+)", line)
+            if length(m.captures) == 3
+                push!(coords, Pair{Int,Int}(m.captures[2], m.captures[3]))
+                continue
+            end
             println(line)
         end
     end
