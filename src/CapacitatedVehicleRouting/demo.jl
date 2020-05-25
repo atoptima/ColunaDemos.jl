d76df66c0c22a7db8ea404d7131a6081edf59298
Guillaume Marques <guillaume.marques@protonmail.com>
2020-05-25 14:08:15 +0200

    model

--- src/CapacitatedVehicleRouting/demo.jl
+++ src/CapacitatedVehicleRouting/demo.jl
@@ -1,6 +1,7 @@
 module CapacitatedVehicleRouting
     using JuMP, BlockDecomposition, LightGraphs, MathOptInterface
     const MOI = MathOptInterface
+    const BD = BlockDecomposition
 
     include("data.jl")
     include("model.jl")
